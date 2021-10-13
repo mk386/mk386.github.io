4d291b7edbdd414fb90064333f48170c8078729e
mk386 <30165533+mk386@users.noreply.github.com>
2021-10-13 16:09:00 -0700

    *: updated benchmark scripts

---
+++ bench/_update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+curl -sL bench.sh > bench.sh
+curl -sL yabs.sh > yabs.sh
+
+curl -s wget.racing/nench.sh > nench.sh
+curl -sL https://raw.githubusercontent.com/K4Y5/ServerBench/master/serverbench.sh > serverbench.sh
+
