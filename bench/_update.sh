6cc6cde9a94c582feebdf84c9b489bc48ab1ad85
mk386 <30165533+mk386@users.noreply.github.com>
2022-05-12 07:37:50 -0700

    *: updated bench scripts

--- bench/_update.sh
+++ bench/_update.sh
@@ -6,3 +6,5 @@ curl -sL yabs.sh > yabs.sh
 curl -s wget.racing/nench.sh > nench.sh
 curl -sL https://raw.githubusercontent.com/K4Y5/ServerBench/master/serverbench.sh > serverbench.sh
 
+curl -sL https://raw.githubusercontent.com/Aniverse/A/i/a > Aniverse.sh
+
