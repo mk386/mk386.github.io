a47b0b145b8df20468781a406012b85bf2204425
mk386 <30165533+mk386@users.noreply.github.com>
2022-06-19 07:25:56 -0700

    *: updated _update.sh script

--- bench/_update.sh
+++ bench/_update.sh
@@ -6,7 +6,7 @@ curl -sL yabs.sh > yabs.sh
 curl -s wget.racing/nench.sh > nench.sh
 curl -sL https://raw.githubusercontent.com/K4Y5/ServerBench/master/serverbench.sh > serverbench.sh
 
-curl -sL https://raw.githubusercontent.com/Aniverse/A/i/a > Aniverse.sh
+curl -sL https://raw.githubusercontent.com/Aniverse/A/i/a > aniverse.sh
 
 
 # bash <(curl -Lso- https://git.io/superspeed)
