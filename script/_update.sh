f1ee7d3be24d6038414b5f250d6940b58753b818
mk386 <30165533+mk386@users.noreply.github.com>
2022-08-16 19:35:15 -0700

    +: added https://github.com/vaxilu/x-ui

--- script/_update.sh
+++ script/_update.sh
@@ -2,4 +2,7 @@
 
 
 curl -sL https://raw.githubusercontent.com/magnific0/wondershaper/master/wondershaper > wondershaper
+curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh > vaxilu_x-ui_install.sh
+
+chmod 755 vaxilu_x-ui_install.sh
 
