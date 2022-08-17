8f05e6c181ec82f824cb5ae685006e3348122df7
mk386 <30165533+mk386@users.noreply.github.com>
2022-08-16 19:42:14 -0700

    +: added https://github.com/acmesh-official

--- script/_update.sh
+++ script/_update.sh
@@ -2,7 +2,11 @@
 
 
 curl -sL https://raw.githubusercontent.com/magnific0/wondershaper/master/wondershaper > wondershaper
-curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh > vaxilu_x-ui_install.sh
 
+
+curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh > vaxilu_x-ui_install.sh
 chmod 755 vaxilu_x-ui_install.sh
+
+curl -sL https://get.acme.sh > acme.sh
+chmod 755 acme.sh
 
